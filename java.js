b54282bf81dc790327358e063000d8474d0fa046
Angela <34062624+AngelaMastenbroek@users.noreply.github.com>
2019-05-23 13:06:50 +0200

    Add files via upload

---
+++ java.js
@@ -0,0 +1,52 @@
+console.log('hello world');
+
+var negative = document.querySelector('.negative');
+var licht = document.querySelector('p');
+
+negative.addEventListener('click',function(){
+    licht.classList.toggle('animate');
+})
+
+
+
+var rotate = document.querySelector('#hadid');
+
+rotate.addEventListener('click',function(){
+    rotate.classList.toggle('animate2');
+})
+
+
+
+var split = document.querySelector('#rison');
+
+split.addEventListener('click',function(){
+    split.classList.toggle('animate3');
+})
+
+
+
+var spin = document.querySelector('#ms');
+
+spin.addEventListener('click',function(){
+    spin.classList.toggle('animate4');
+})
+
+
+
+var spring = document.getElementById('newYork');
+
+document.addEventListener('keyup', logKey);
+
+function logKey(e) {
+  spring.classList.toggle('animate5');
+}
+
+
+
+
+
+/*
+bronnen van code:
+https://developer.mozilla.org/en-US/docs/Web/API/Document/keyup_event
+
+*/
